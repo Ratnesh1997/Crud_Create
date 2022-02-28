07c1cf22443c72508c24a3c8667732d57c5a6708
admin <admin@CHETUIWK217>
2022-02-28 14:41:40 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using CrudR.Data_Access;
+using CrudR.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudR.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public DatabaseConn _DBCONN { get; }
+        public CustomerController(DatabaseConn DBCONN)
+        {
+            this._DBCONN = DBCONN;
+        }
+
+        public IActionResult Index()
+        {
+            //List<Customer> data = _DBCONN.Customers.ToList();
+            var data = _DBCONN.Customers.ToList();
+            return View(data);
+        }
+        public IActionResult Create()
+        {            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Customer customers)
+        {
+            _DBCONN.Customers.Add(customers);
+            _DBCONN.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
